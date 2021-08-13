2b299b212da852772cbfe408bfe2260458dcc92d
Stephany Ramos <stephanytephany177@gmail.com>
2021-08-13 11:07:31 -0300

    finish

---
+++ salario.php
@@ -0,0 +1,39 @@
+<?php
+if (isset ($_POST["nome"]) && isset($_POST["salarioAtual"])
+ && isset($_POST["genero"]) && isset($_POST["cargo"]))
+{
+$nome = $_POST["nome"];
+$salarioAtual = $_POST["salarioAtual"];
+$genero = $_POST["genero"];
+$cargo = $_POST["cargo"];
+$salarioNovo = 0;
+} else {
+die ("<main>Campos não preenchidos</main>");
+} 
+// if ($salarioAtual > 5000) {
+ 
+// $salarioNovo = $salarioAtual * 1.1;
+// } else {
+// $salarioNovo = $salarioAtual * 1.2;
+// }
+$salarioNovo = $salarioAtual > 5000 ? $salarioAtual * 1.1 : $salarioAtual * 1.2;
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style.css">
+<title>Calcular salario</title>
+</head>
+<body>
+<main>
+<p>
+<?= $genero === "m" ? "O" : ($genero === "f" ? "A" : "") ?> <?= $nome ?> passará a receber R$
+<?= number_format($salarioNovo, 2, ",", ".") ?>, 
+ no cargo de <?= $cargo ?>
+</p>
+</main>
+</body>
+</html>
